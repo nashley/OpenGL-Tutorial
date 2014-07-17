38e3fad871cddbc18cb7234c32abe6638eb43b10
Evets <vreality2007@gmail.com>
2014-07-16 21:14:21 -0400

    Organization and comments for 1-1 Comments for the functions of 1-1; organized function order in code to match run order

--- 01/1-1.c
+++ 01/1-1.c
@@ -15,41 +15,26 @@
 
 GLFWwindow* window;
 
+/*
+	Output errors to stderr
+*/
 static void error_callback(int error, const char* description){
 
 	fputs(description, stderr);
 }
 
+/*
+	Process key events
+*/
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-void quit(){
-
-	printf("Exiting...\n");
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
-}
-
-void draw(){
-
-	glClearColor (0.0, 0.0, 0.0, 0.0);
-	glClear (GL_COLOR_BUFFER_BIT);
-	glColor3f (1.0, 1.0, 1.0);
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-
-	glBegin(GL_POLYGON);
-		glVertex3f (0.25, 0.25, 0.0);
-		glVertex3f (0.75, 0.25, 0.0);
-		glVertex3f (0.75, 0.75, 0.0);
-		glVertex3f (0.25, 0.75, 0.0);
-	glEnd();
-	glFlush();
-}
-
+/*
+	Set up GLFW and our window, including key listeners
+*/
 void init(){
 
 	glfwSetErrorCallback(error_callback);
@@ -66,8 +51,28 @@ void init(){
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
 }
+
 /*
-	Loop the window
+	Prepare and draw the image to be rendered
+*/
+void draw(){
+
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glClear (GL_COLOR_BUFFER_BIT);
+	glColor3f (1.0, 1.0, 1.0);
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+
+	glBegin(GL_POLYGON);
+		glVertex3f (0.25, 0.25, 0.0);
+		glVertex3f (0.75, 0.25, 0.0);
+		glVertex3f (0.75, 0.75, 0.0);
+		glVertex3f (0.25, 0.75, 0.0);
+	glEnd();
+	glFlush();
+}
+
+/*
+	Loop the window (listen to events while the window should not close)
 */
 int run(){
 
@@ -75,6 +80,17 @@ int run(){
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+}
+
+/*
+	Shutdown and quit the program
+*/
+void quit(){
+
+	printf("Exiting...\n");
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
 
 int main(){
